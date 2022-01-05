e0c1ddd344437b4ffacbc63b45abab87181301b1
Tesseract <tensai3355@gmail.com>
2022-01-05 21:38:10 +0900

    Add footer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const header = document.querySelector('.doc-header');
 
 window.addEventListener('scroll', () => {
-  console.log('ㅇㅇ');
   if (window.scrollY > 0) {
     header.classList.add('header-bottom');
   } else {
